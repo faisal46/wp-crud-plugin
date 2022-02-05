e307f2c6068952f71cf2a2b84a06f7eb2de22f1b
rajtechbdt <rajtechbdt@gmail.com>
2022-02-05 23:14:35 +0600

    1st commit

---
+++ faisal-academy.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Faisal Academy
+ * Description: Rest API plugin for Faisal Academy
+ * Plugin URI: https://www.faisal.rajtechbd.com/
+ * Author: Md. Faisal Amir Mostafa
+ * Author URI: https://www.faisal.rajtechbd.com/
+ * Version: 1.0
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+ echo "Faisal amir mostafa dfdf ";
